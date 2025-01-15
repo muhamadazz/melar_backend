b97a0a79a9a46cdd68902d322f78ecebed3b5a08
muhamadazz <153073061+muhamadazz@users.noreply.github.com>
2025-01-15 21:03:58 +0700

    Update settings.py

--- melar_project/settings.py
+++ melar_project/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
 }
 
-CSRF_TRUSTED_ORIGINS = ["https://melarbackend-production.up.railway.app/"]
+CSRF_TRUSTED_ORIGINS = ["https://melarbackend-production.up.railway.app"]
 
 
 # Password validation
