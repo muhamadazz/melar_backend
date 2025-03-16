f8394957fd063356dcbd7087ea6c058d604901c8
jelimutaalidev <jelimutaalidev@gmail.com>
2025-03-16 08:08:26 +0700

    memperbarui fitur

--- rentals/admin.py
+++ rentals/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Cart, Order, Shipping, OrderItem
 
-# Register your models here.
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'quantity', 'total_price')
+    search_fields = ('user__username', 'product__name')
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'total_price', 'borrow_date', 'return_deadline', 'status', 'created_at')
+    list_filter = ('status', 'borrow_date', 'return_deadline')
+    search_fields = ('user__username',)
+
+@admin.register(Shipping)
+class ShippingAdmin(admin.ModelAdmin):
+    list_display = ('order', 'user_name', 'address', 'postal_code', 'phone_number')
+    search_fields = ('user_name', 'order__id')
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'quantity', 'total_price')
+    search_fields = ('order__id', 'product__name')
--- shops/admin.py
+++ shops/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Shop, Category, Product, Discount, Inventory
 
-# Register your models here.
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('shop_name', 'user', 'is_active', 'address', 'contact', 'created_at')
+    search_fields = ('shop_name', 'user__username', 'address')
+    list_filter = ('is_active',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at')
+    search_fields = ('name',)
+    ordering = ('-created_at',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'shop', 'get_owner', 'price', 'availability_status', 'status', 'created_at')
+    search_fields = ('name', 'shop__shop_name', 'shop__user__username')
+    list_filter = ('availability_status', 'status', 'categories')
+    ordering = ('-created_at',)
+
+    def get_owner(self, obj):
+        return obj.shop.user.username  # Mengambil username pemilik toko
+    get_owner.short_description = "Owner"  # Nama kolom di Django Admin
+
+@admin.register(Discount)
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ('code', 'percentage', 'valid_from', 'valid_until', 'admin', 'product', 'category')
+    search_fields = ('code', 'admin__username')
+    list_filter = ('valid_from', 'valid_until')
+
+@admin.register(Inventory)
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ('product', 'quantity', 'created_at')
+    search_fields = ('product__name',)
